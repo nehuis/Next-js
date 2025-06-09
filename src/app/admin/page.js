2a58dfcef9f2feff8021c712d3348456c63e4591
Nehuel Caraballo <nehuelcaraballo@gmail.com>
2025-06-09 16:28:07 -0300

    useEffect & fetch

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,16 +1,21 @@
+"use client";
+import { useEffect, useState } from "react";
 import { ProductsTable } from "./components/ProductTable";
 
-export default async function AdminPage() {
-  const products = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/products/all`
-  ).then((res) => res.json());
-  return (
-    <>
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
-        <h1 className="text-2xl font-bold mb-6">Admin Section</h1>
+export default function AdminPage() {
+  const [products, setProducts] = useState([]);
 
-        <ProductsTable products={products} />
-      </div>
-    </>
+  useEffect(() => {
+    fetch("/api/products/all")
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+      .catch((err) => console.error("Error al hacer fetch:", err));
+  }, []);
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-2xl font-bold mb-6">Admin Section</h1>
+      <ProductsTable products={products} />
+    </div>
   );
 }
